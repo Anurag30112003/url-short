111c72b3533401a0ffa3d06de0da455bd55c8f4a
Anurag <anuragsharma011011@gmail.com>
2022-08-16 01:50:09 +0530

    redirect ??

---
+++ src/pages/api/redirect/[slug].ts
@@ -0,0 +1,28 @@
+import { prisma } from "../../../db/client";
+import { useRouter } from "next/router";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const router = useRouter();
+
+  const { slug } = router.query as { slug: string };
+
+  if (!slug) {
+    return res.status(400).send("No slug provided");
+  }
+
+  const exists = prisma.url.findMany({
+    where: {
+      slug,
+    },
+    select: {
+      url: true,
+    },
+  });
+
+  if ((await exists).length > 0) {
+    const url = (await exists)[0].url;
+    router.push(url);
+    res.send("Redirect successfull");
+  }
+};
