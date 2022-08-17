22c4f05f609450c1248274b87f50e2be33826bea
Anurag <anuragsharma011011@gmail.com>
2022-08-18 03:07:32 +0530

    random slug

--- src/pages/api/url/create.ts
+++ src/pages/api/url/create.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../db/client";
+import makeid from "../../../createId";
 
 // create url
 
@@ -7,41 +8,32 @@ export default async function createUrl(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { link, slug,email } = req.body;
-  const exists = prisma.link.findMany({
-    where: {
-      slug,
-    },
-  });
-  
-  // check if url is correct
-  
-  if(link.startsWith("http://") || link.startsWith("https://")){
-    
-    if ((await exists).length > 0) {
-      res.status(400).send("Url already exists");
-    } else {
-      const Url = await prisma.link.create({
-        data: {
-          url: link,
-          slug,
-        },
-         
-      });
-      
-      res.status(201).json(Url);
-      // add link to user model
-      const user = await prisma.user.findMany({
-        where: {
-          email,
-        },
-      }) 
-      if(user.length > 0){
-          
-      }
+  const { link, email } = req.body;
+  const slug = makeid(5);
+  // const data = prisma.link.findMany({
+  //   where: {
+  //     slug,
+  //   },
+  // });
+
+  if (link.startsWith("http://") || link.startsWith("https://")) {
+    const Url = await prisma.link.create({
+      data: {
+        url: link,
+        slug,
+      },
+    });
+
+    res.status(201).json(Url);
+    // add link to user model
+    const user = await prisma.user.findMany({
+      where: {
+        email,
+      },
+    });
+    if (user.length > 0) {
+    }
+  } else {
+    res.status(400).send("Url is not correct");
   }
-}
-else{
-  res.status(400).send("Url is not correct");
-}
 }
